9b1d378d2cae44643e8f1a84bb2e56eed4ace81d
Rutger Kok <rutger.kok@live.com>
2019-04-28 17:17:09 +0200

    Fix biome scaling

--- worldgeneratorapi-impl/src/main/java/nl/rutgerkok/worldgeneratorapi/internal/BiomeGeneratorImpl.java
+++ worldgeneratorapi-impl/src/main/java/nl/rutgerkok/worldgeneratorapi/internal/BiomeGeneratorImpl.java
@@ -7,6 +7,7 @@ import org.bukkit.craftbukkit.v1_14_R1.block.CraftBlock;
 
 import net.minecraft.server.v1_14_R1.BiomeBase;
 import net.minecraft.server.v1_14_R1.WorldChunkManager;
+
 import nl.rutgerkok.worldgeneratorapi.BiomeGenerator;
 
 public final class BiomeGeneratorImpl implements BiomeGenerator {
@@ -30,12 +31,11 @@ public final class BiomeGeneratorImpl implements BiomeGenerator {
 
     @Override
     public Biome[] getZoomedOutBiomes(int minX, int minZ, int xSize, int zSize) {
-        // Last parameter doesn't seem to be used anywhere V
-        BiomeBase[] biomeArray = internal.a(minX, minZ, xSize, zSize, false);
-
         Biome[] biomes = new Biome[xSize * zSize];
         for (int i = 0; i < xSize * zSize; i++) {
-            biomes[i] = CraftBlock.biomeBaseToBiome(biomeArray[i]);
+            int xLocal = i % xSize;
+            int zLocal = i / xSize;
+            biomes[i] = CraftBlock.biomeBaseToBiome(internal.b(minX + xLocal, minZ + zLocal));
         }
         return biomes;
     }
